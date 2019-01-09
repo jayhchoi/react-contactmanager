258fce23b265dc2310b54c055c36d0f32ed01487
JayHChoi <jaychoi1619@gmail.com>
2019-01-09 14:55:04 +0900

    Updates

---
+++ precache-manifest.de871cb6886f7e3e2a56c7f7ca6b106d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3fff7a80babed061b04",
+    "url": "/react-contactmanager/static/js/main.e3fff7a8.chunk.js"
+  },
+  {
+    "revision": "9e82e550216ee73dff89",
+    "url": "/react-contactmanager/static/css/1.898d815b.chunk.css"
+  },
+  {
+    "revision": "9e82e550216ee73dff89",
+    "url": "/react-contactmanager/static/js/1.9e82e550.chunk.js"
+  },
+  {
+    "revision": "3e40d23b8b61876c686f",
+    "url": "/react-contactmanager/static/js/runtime~main.3e40d23b.js"
+  },
+  {
+    "revision": "73d472543e408f9cf49eb50734340b8d",
+    "url": "/react-contactmanager/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-contactmanager/precache-manifest.de871cb6886f7e3e2a56c7f7ca6b106d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-contactmanager/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
